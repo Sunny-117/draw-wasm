678160b42e1cd6958c520d5fa95e22d54072b3ed
Sunny-117 <zhiqiangfu6@gmail.com>
2026-03-25 12:21:07 +0800

    chore: deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+pnpm build
+
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/Sunny-117/draw-wasm.git main:gh-pages
+
+cd -
